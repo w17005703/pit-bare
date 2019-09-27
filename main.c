74e2bd7356bccd0ef79e77e3f8376106677bee36
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-09-27 14:19:05 +0100

    Solution 5: using a switch interrupt

--- main.c
+++ main.c
@@ -2,22 +2,22 @@
 #include <stdbool.h>
 #include "MK64F12.h"
 
+#define PIN6_MASK            (1u << 6)
 #define PIN21_MASK           (1u << 21)
-#define PIN22_MASK           (1u << 22)
-#define PIN26_MASK           (1u << 26)
 
-void leds_init(void);
+static bool flashing = true;
+
+void blue_init(void);
 void blue_toggle(void);
-void red_toggle(void);
-void green_toggle(void);
+void sw2_init(void);
 void PIT_init(void);
 void PIT0_IRQHandler(void);
-void PIT1_IRQHandler(void);
-void PIT2_IRQHandler(void);
+void PORTC_IRQHandler(void);
 
 int main(void)
 {
-    leds_init();
+    blue_init();
+    sw2_init();
     PIT_init();
     while (true) {
     }
@@ -25,45 +25,42 @@ int main(void)
     return 0;
 }
 
-void leds_init(void) {
-    /* Enable the clock to PORTs B and E */
-    SIM_SCGC5 |= (SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTE_MASK);
+void blue_init(void) {
+    /* Enable the clock to PORT B */
+    SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
 
     /* Select the GPIO function (Alternative 1) for pin 21 of PORT B */
     PORTB_PCR21 &= ~PORT_PCR_MUX_MASK;
     PORTB_PCR21 |= (1u << PORT_PCR_MUX_SHIFT);
-    /* Select the GPIO function (Alternative 1) for pin 22 of PORT B */
-    PORTB_PCR22 &= ~PORT_PCR_MUX_MASK;
-    PORTB_PCR22 |= (1u << PORT_PCR_MUX_SHIFT);
-    /* Select the GPIO function (Alternative 1) for pin 26 of PORT E */
-    PORTE_PCR26 &= ~PORT_PCR_MUX_MASK;
-    PORTE_PCR26 |= (1u << PORT_PCR_MUX_SHIFT);
 
     /* Set the data direction for pin 21 of PORT B to output */
     GPIOB_PDDR |= PIN21_MASK;
-    /* Set the data direction for pin 22 of PORT B to output */
-    GPIOB_PDDR |= PIN22_MASK;
-    /* Set the data direction for pin 26 of PORT E to output */
-    GPIOE_PDDR |= PIN26_MASK;
 
-    /* LEDs off initially */
+    /* LED off initially */
     GPIOB_PDOR |= PIN21_MASK;
-    GPIOB_PDOR |= PIN22_MASK;
-    GPIOE_PDOR |= PIN26_MASK;
 }
 
-void red_toggle(void) {
-    /* Red LED, ON <- OFF, OFF <- ON */
-    GPIOB_PDOR ^= PIN22_MASK; 
-}
-
-void green_toggle(void) {
-    /* Green LED, ON <- OFF, OFF <- ON */
-    GPIOE_PDOR ^= PIN26_MASK; 
-}
 void blue_toggle(void) {
-    /* Blue LED, ON <- OFF, OFF <- ON */
-    GPIOB_PDOR ^= PIN21_MASK; 
+    if (flashing) {
+        /* Blue LED, ON <- OFF, OFF <- ON */
+        GPIOB_PDOR ^= PIN21_MASK;
+    }
+}
+
+void sw2_init(void) {
+    /* Enable the clock to PORT C */
+    SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+
+    /* Select the GPIO function (Alternative 1) for pin 6 of PORT C */
+    PORTC_PCR6 &= ~PORT_PCR_MUX_MASK;
+    PORTC_PCR6 |= (1u << PORT_PCR_MUX_SHIFT);
+    /* Select interrupt trigger on rising edge - button release */
+    PORTC_PCR6 &= ~PORT_PCR_IRQC_MASK;
+    PORTC_PCR6 |= (9u << PORT_PCR_IRQC_SHIFT);
+    /* Set the data direction for pin 6 of PORT C to input */
+    GPIOC_PDDR &= ~PIN6_MASK;
+    /* Enable the interrupt in the NVIC */
+    NVIC_EnableIRQ(PORTC_IRQn);
 }
 
 void PIT_init(void) {
@@ -71,53 +68,29 @@ void PIT_init(void) {
     SIM_SCGC6 |= (1u << 23);
     /* Enable the clock for the PIT timers. Continue to run in debug mode */
     PIT_MCR_REG(PIT) = 0u;
-
     /* Period p = 0.5 s, bus clock f = 60 MHz, v = pf - 1 */ 
     PIT_LDVAL_REG(PIT, 0) = 29999999;
     /* Enable interrupt on timeout */
     PIT_TCTRL_REG(PIT, 0) |= PIT_TCTRL_TIE_MASK;
     /* Enable the interrupt in the NVIC */
     NVIC_EnableIRQ(PIT0_IRQn);
-
-    /* Period p = 0.2 s, bus clock f = 60 MHz, v = pf - 1 */ 
-    PIT_LDVAL_REG(PIT, 1) = 11999999;
-    /* Enable interrupt on timeout */
-    PIT_TCTRL_REG(PIT, 1) |= PIT_TCTRL_TIE_MASK;
-    /* Enable the interrupt in the NVIC */
-    NVIC_EnableIRQ(PIT1_IRQn);
-
-    /* Period p = 0.33... s, bus clock f = 60 MHz, v = pf - 1 */ 
-    PIT_LDVAL_REG(PIT, 2) = 19999999;
-    /* Enable interrupt on timeout */
-    PIT_TCTRL_REG(PIT, 2) |= PIT_TCTRL_TIE_MASK;
-    /* Enable the interrupt in the NVIC */
-    NVIC_EnableIRQ(PIT2_IRQn);
-
     /* Start the timer running */
     PIT_TCTRL_REG(PIT, 0) |= PIT_TCTRL_TEN_MASK;
-    /* Start the timer running */
-    PIT_TCTRL_REG(PIT, 1) |= PIT_TCTRL_TEN_MASK;
-    /* Start the timer running */
-    PIT_TCTRL_REG(PIT, 2) |= PIT_TCTRL_TEN_MASK;
 }
 
 void PIT0_IRQHandler(void) {
     /* Call the main handler function */
     blue_toggle();
     /* Clear the timer interrupt flag to allow further timer interrupts */
-    PIT_TFLG_REG(PIT, 0) |= PIT_TFLG_TIF_MASK;
+    PIT_TFLG_REG(PIT,0) |= PIT_TFLG_TIF_MASK;
 }
 
-void PIT1_IRQHandler(void) {
-    /* Call the main handler function */
-    red_toggle();
-    /* Clear the timer interrupt flag to allow further timer interrupts */
-    PIT_TFLG_REG(PIT, 1) |= PIT_TFLG_TIF_MASK;
+void PORTC_IRQHandler(void) {
+    if (PORTC_ISFR & PIN6_MASK) {
+        /* Toggle the flashing state */
+        flashing = !flashing;
+        /* Clear the interrupt flag */
+        PORTC_ISFR |= PIN6_MASK;
+    }
 }
 
-void PIT2_IRQHandler(void) {
-    /* Call the main handler function */
-    green_toggle();
-    /* Clear the timer interrupt flag to allow further timer interrupts */
-    PIT_TFLG_REG(PIT, 2) |= PIT_TFLG_TIF_MASK;
-}
